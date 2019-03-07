c9fd343c21626202105710aa17c4e280ad8fea57
Mike Bifulco <mbifulco@live.com>
2019-03-07 14:32:38 -0500

    some name changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ require('dotenv').config();
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gymnasium`,
+    description: `Gymnasium front end`,
+    author: `@irreverentmike`,
   },
   plugins: [
     {
