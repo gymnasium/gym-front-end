fd8f7cfef23ead1cc4367246b90a87136e8c8074
Mike Bifulco <mbifulco@live.com>
2019-03-07 14:30:36 -0500

    setup robots.txt

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,6 +47,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-robots-txt',
+      options: {
+        host: 'https://www.example.com',
+        sitemap: 'https://www.example.com/sitemap.xml',
+        policy: [{ userAgent: '*', disallow: '/' }],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
