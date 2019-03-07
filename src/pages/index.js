c919a60e5f83c2d4b19d5071faa5c1c01a71eab6
Mike Bifulco <mbifulco@live.com>
2019-03-07 14:32:38 -0500

    some name changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ import { GymButton } from '../components';
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
+    <h1>Gymnasium</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
