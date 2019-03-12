e9c4de5d62c1f5c938484cf05a2b5514b386ff61
Mike Bifulco <mbifulco@live.com>
2019-03-12 13:46:36 -0400

    add jQuery/Bootstrap to gatsby project

--- src/html.js
+++ src/html.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const jquery = (
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" />
-);
-
-const bootstrap = (
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" />
+// add scripts for jQuery and Bootstrap
+const jQuery = (
+  <script
+    src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+    integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
+    crossOrigin="anonymous"
+  />
 );
 
 const bootstrapCSS = (
@@ -14,6 +15,14 @@ const bootstrapCSS = (
     rel="stylesheet"
     href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css"
     integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu"
+    crossOrigin="anonymous"
+  />
+);
+
+const bootstrapJS = (
+  <script
+    src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"
+    integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd"
     crossOrigin="anonymous"
   />
 );
@@ -28,9 +37,9 @@ export default function HTML(props) {
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no"
         />
-        {jquery}
+        {jQuery}
         {bootstrapCSS}
-        {bootstrap}
+        {bootstrapJS}
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
