c943bf0be87c7435bc9923ed8dd63b0fcd377b24
Mike Bifulco <mbifulco@live.com>
2019-03-19 13:30:23 -0400

    specify a class here to unbreak modules

--- src/components/Link/Link.js
+++ src/components/Link/Link.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import './Link.module.css';
+import classes from './Link.module.css';
 
-const Link = ({ children, to }) => <a href={to}>{children}</a>;
+const Link = ({ children, to }) => (
+  <a className={classes.link} href={to}>
+    {children}
+  </a>
+);
 
 Link.defaultProps = {
   to: '#',
