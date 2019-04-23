c56565218858281c9b214d3532dbb9e0733ade93
Mike Bifulco <mbifulco@aquent.com>
2019-04-23 13:41:58 -0400

    move image component to components/Image directory
    this will enable easier importing soon

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'gatsby';
 
 import Layout from '../components/layout';
-import Image from '../components/image';
+import { Image } from '../components/Image';
 import SEO from '../components/seo';
 import { GymButton } from '../components';
 
