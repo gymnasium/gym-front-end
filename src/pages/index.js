1bee7b866ab6091afbcbf3f9f409b942acfff929
Mike Bifulco <mbifulco@aquent.com>
2019-04-23 13:51:17 -0400

    👋 goodbye, page-2

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,6 @@ const IndexPage = () => (
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
     <GymButton>Hello</GymButton>
   </Layout>
 );
