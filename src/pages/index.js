56a15d44de0af2e2a2f21e86a4131cd7df5bdc45
Mike Bifulco <mbifulco@aquent.com>
2019-04-23 15:11:33 -0400

    use featured course component!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,66 @@
 import React from 'react';
+import { graphql } from 'gatsby';
 import { Col, Container, Row } from 'react-bootstrap';
 
-import { GymButton, Image, Layout, SEO } from '../components';
+import { FeaturedCourse, GymButton, Layout, SEO } from '../components';
 
 import classes from './HomePage.module.css';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <Container fluid>
-      <Container className={classes.container}>
-        <Row>
-          <Col>
-            <GymButton>Hello</GymButton>
-            <h1>test</h1>
-          </Col>
-        </Row>
+const IndexPage = ({ data }) => {
+  const { courses } = data.takeshape;
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <Container fluid>
+        <Container className={classes.container}>
+          <Row>
+            <Col>
+              <GymButton>Hello</GymButton>
+              <h1>test</h1>
+            </Col>
+          </Row>
+
+          <section id="featured-courses" className={classes.featuredCourses}>
+            <ul>
+              {courses.items.map(course => (
+                <FeaturedCourse key={course._id} course={course} />
+              ))}
+            </ul>
+          </section>
+        </Container>
       </Container>
-    </Container>
-  </Layout>
-);
+    </Layout>
+  );
+};
+
+export const query = graphql`
+  {
+    takeshape {
+      courses: getFullCourseList {
+        items {
+          author {
+            displayName
+            workplace
+            photo {
+              path
+            }
+          }
+          title
+          descriptionHtml
+          courseNumber
+          courseType {
+            displayName
+            color {
+              hex
+            }
+          }
+          coverImage {
+            path
+          }
+        }
+      }
+    }
+  }
+`;
 
 export default IndexPage;
