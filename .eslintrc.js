afc0ad20ef532be37eb69651e02989e2b49ef6e1
Mike Bifulco <mbifulco@aquent.com>
2019-09-18 12:00:15 -0400

    add and configure lint/dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  globals: {
+    __PATH_PREFIX__: true,
+  },
+  extends: [
+    'airbnb',
+    'react-app',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+  ],
+};
