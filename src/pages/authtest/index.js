720c20a3c17a21587d05ed7a715e2382579b096a
root <root@mike-at-mikes-mbp-local.us-central1-c.c.gymnasium-devstack.internal>
2020-04-16 17:10:11 +0000

    auth stuff

---
+++ src/pages/authtest/index.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from 'react';
+import {
+  configure,
+  fetchAuthenticatedUser,
+  getAuthenticatedHttpClient
+} from '@edx/frontend-platform/auth';
+import { getConfig } from '@edx/frontend-platform';
+// import { getLoggingService } from '@edx/frontend-platform/logging';
+
+const {
+  BASE_URL,
+  LMS_BASE_URL,
+  LOGIN_URL,
+  LOGOUT_URL,
+  REFRESH_ACCESS_TOKEN_ENDPOINT,
+  ACCESS_TOKEN_COOKIE_NAME,
+  CSRF_TOKEN_API_PATH,
+} = getConfig();
+
+configure({
+  // loggingService: getLoggingService(),
+  appBaseUrl: BASE_URL,
+  lmsBaseUrl: LMS_BASE_URL,
+  loginUrl: LOGIN_URL,
+  logoutUrl: LOGOUT_URL,
+  refreshAccessTokenEndpoint: REFRESH_ACCESS_TOKEN_ENDPOINT,
+  accessTokenCookieName: ACCESS_TOKEN_COOKIE_NAME,
+  csrfTokenApiPath: CSRF_TOKEN_API_PATH,
+});
+
+const AuthPage = () => {
+  const [authenticatedUser, setAuthenticatedUser] = useState(async () => await fetchAuthenticatedUser()); // validates and decodes JWT token
+  const [authenticatedHttpClient, setAuthenticatedHttpClient] = useState(() => getAuthenticatedHttpClient());
+  const [currentUser, setCurrentUser] = useState();
+
+  const getAuthenticatedUser = async () => {
+    const response = await authenticatedHttpClient.get(`http://edx.devstack.lms:18000/api/user/data/${authenticatedUser.username}`); // fetching from an authenticated API using user data
+    setCurrentUser(response);
+  };
+
+  useEffect(()=>{    
+    getAuthenticatedUser();
+  },[setCurrentUser]);
+  return (
+    <>
+      <h1>Auth page!</h1>
+      <h2>Current user is {currentUser ? currentUser.displayName : 'loading'}</h2>
+    </>
+  );
+};
+
+export default AuthPage;
---
+++ src/utils/auth.js
@@ -0,0 +1,32 @@
+import {
+  configure,
+  fetchAuthenticatedUser,
+  getAuthenticatedHttpClient
+} from '@edx/frontend-platform/auth';
+import { getConfig } from '@edx/frontend-platform';
+import { getLoggingService } from '@edx/frontend-platform/logging';
+
+const {
+  BASE_URL,
+  LMS_BASE_URL,
+  LOGIN_URL,
+  LOGOUT_URL,
+  REFRESH_ACCESS_TOKEN_ENDPOINT,
+  ACCESS_TOKEN_COOKIE_NAME,
+  CSRF_TOKEN_API_PATH,
+} = getConfig();
+
+configure({
+  loggingService: getLoggingService(),
+  appBaseUrl: BASE_URL,
+  lmsBaseUrl: LMS_BASE_URL,
+  loginUrl: LOGIN_URL,
+  logoutUrl: LOGOUT_URL,
+  refreshAccessTokenEndpoint: REFRESH_ACCESS_TOKEN_ENDPOINT,
+  accessTokenCookieName: ACCESS_TOKEN_COOKIE_NAME,
+  csrfTokenApiPath: CSRF_TOKEN_API_PATH,
+});
+
+const authenticatedUser = await fetchAuthenticatedUser(); // validates and decodes JWT token
+const authenticatedHttpClient = getAuthenticatedHttpClient();
+const response = await getAuthenticatedHttpClient().get(`https://example.com/api/user/data/${authenticatedUser.username}`); // fetching from an authenticated API using user data
