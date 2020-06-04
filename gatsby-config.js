eb455ad5281a6c4e3c2ffe1193b9e34a37d3609f
Mike Bifulco <mbifulco@live.com>
2020-06-04 16:47:02 -0400

    deps + netlify plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,6 +85,7 @@ module.exports = {
       },
     },
     `gatsby-plugin-mdx`,
+    `gatsby-plugin-netlify`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     // 'gatsby-plugin-offline',
