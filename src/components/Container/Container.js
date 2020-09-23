85b88f398ce7f883f0192d153afbe018b7e6e98e
Mike Bifulco <mbifulco@live.com>
2020-09-23 15:44:44 -0400

    updates to container

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,15 +2,25 @@
 import React from 'react';
 
 import { useMediaQuery } from 'react-responsive';
-import { Container as BootstrapContainer } from 'react-bootstrap';
+import { Box } from '@chakra-ui/core';
 
 const Container = ({ children, fluid, ...rest }) => {
   const isSmallViewport = useMediaQuery({ query: '(max-width: 992px)' });
 
+  const containerWidth = isSmallViewport || fluid ? '100vw' : undefined;
+
+  const maxWidth = fluid ? '100vw' : '1170px';
+
   return (
-    <BootstrapContainer {...rest} fluid={isSmallViewport || fluid}>
+    <Box
+      maxWidth={maxWidth}
+      width={containerWidth}
+      {...rest}
+      ml="auto"
+      mr="auto"
+    >
       {children}
-    </BootstrapContainer>
+    </Box>
   );
 };
 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,21 +3,26 @@ import React from 'react';
 import { Flex, useTheme } from '@chakra-ui/core';
 import { TwitchHero } from './TwitchHero';
 
+import { Container } from '..';
+
 const Hero = ({ isTwitchChannelActive }) => {
   const theme = useTheme();
 
   if (isTwitchChannelActive) return <TwitchHero />;
 
   return (
-    <Flex
-      backgroundColor="#d73158"
-      marginLeft="-15px"
-      minHeight="325px"
-      direction="row"
-      justifyContent="center"
-      alignItems="center"
-      color={theme.colors.white}
-    />
+    <Container fluid backgroundColor="#d73158" minHeight="325px">
+      <Container>
+        <Flex
+          direction="row"
+          justifyContent="center"
+          alignItems="center"
+          color={theme.colors.white}
+        >
+          Hello, world.
+        </Flex>
+      </Container>
+    </Container>
   );
 };
 
