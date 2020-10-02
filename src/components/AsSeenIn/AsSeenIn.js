987d5e183370ae52065ecf714189c012b97bb58e
Mike Bifulco <mbifulco@live.com>
2020-10-02 19:39:12 -0400

    simplify As Seen In

--- src/components/AsSeenIn/AsSeenIn.js
+++ src/components/AsSeenIn/AsSeenIn.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { getImageUrl } from 'takeshape-routing';
-import { Col, Row } from 'react-bootstrap';
 import { OutboundLink } from 'gatsby-plugin-google-analytics';
+
+import { SimpleGrid } from '@chakra-ui/core';
 
 import sortBy from 'lodash/sortBy';
 
@@ -15,33 +16,22 @@ const AsSeenIn = ({ items }) => (
     <H2 className={classes.title} textAlign="center">
       As Seen In:
     </H2>
-    <Row noGutters>
-      <Col xs={12}>
-        <Row noGutters>
-          {sortBy(items, item => item.order).map((item, idx) => (
-            <Col
-              className="text-center"
-              key={item._id}
-              xs={12}
-              sm={4}
-              lg={{ span: 2, offset: idx === 0 ? 1 : 0 }}
-            >
-              <OutboundLink
-                href={item.url}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={classes.featuredImage}
-              >
-                <img
-                  src={getImageUrl(item.publicationLogo.path)}
-                  alt={item.publicationLogo.title}
-                />
-              </OutboundLink>
-            </Col>
-          ))}
-        </Row>
-      </Col>
-    </Row>
+    <SimpleGrid columns={[1, 2, 5]} alignItems="space-between">
+      {sortBy(items, item => item.order).map((item, idx) => (
+        <OutboundLink
+          key={item._id}
+          href={item.url}
+          target="_blank"
+          rel="noopener noreferrer"
+          className={classes.featuredImage}
+        >
+          <img
+            src={getImageUrl(item.publicationLogo.path)}
+            alt={item.publicationLogo.title}
+          />
+        </OutboundLink>
+      ))}
+    </SimpleGrid>
   </div>
 );
 
